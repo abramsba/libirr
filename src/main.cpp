20811169a4eb97e1ac00e5c9670931a8a8909009
Bryan Abrams <abrams.b.a@gmail.com>
2019-03-31 22:57:07 +0200

    Included majority of add node functions and created module export

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,4 @@
-
+#include <iostream>
 #include <irrlicht.h>
 using namespace irr;
 using namespace core;
@@ -41,6 +41,13 @@ extern "C" {
 		return device->run();
 	}
 
+	void setWindowCaption(IrrlichtDevice * device, char * title) {
+		std::string titleString(title);
+		std::wstring wTitle(titleString.length(), L' ');
+		std::copy(titleString.begin(), titleString.end(), wTitle.begin());
+		device->setWindowCaption(wTitle.c_str());
+	}
+
 	// Video Driver
 
 	IVideoDriver * getVideoDriver(IrrlichtDevice * device) {
@@ -55,10 +62,88 @@ extern "C" {
 		driver->endScene();
 	}
 
+	int getFPS(IVideoDriver * driver) {
+		return driver->getFPS();
+	}
+	
+	ITexture * getTexture(IVideoDriver * driver, char * filepath) {
+		return driver->getTexture(filepath);
+	}
+
 	// Scene Manager
 
 	ISceneManager * getSceneManager(IrrlichtDevice * device) {
 		return device->getSceneManager();
+	}
+
+	void drawScene(ISceneManager * scene) {
+		scene->drawAll();
+	}
+
+	IAnimatedMesh * getMesh(ISceneManager * scene, char * filepath) {
+		return scene->getMesh(filepath);
+	}
+
+	IAnimatedMeshSceneNode * addAnimatedMesh(ISceneManager * scene, IAnimatedMesh * mesh) {
+		return scene->addAnimatedMeshSceneNode(mesh);
+	}
+
+	IBillboardSceneNode * addBillboard(ISceneManager * scene) {
+		return scene->addBillboardSceneNode();
+	}
+
+	ISceneNode * addNull(ISceneManager * scene) {
+		return scene->addEmptySceneNode();
+	}
+
+	ILightSceneNode * addLight(ISceneManager * scene) {
+		return scene->addLightSceneNode();
+	}
+
+	ISceneNode * addCube(ISceneManager * scene) {
+		return scene->addCubeSceneNode();
+	}
+
+	IMeshSceneNode * addOctree(ISceneManager * scene, IAnimatedMesh * mesh) {
+		return scene->addOctreeSceneNode(mesh);
+	}
+
+	ISceneNode * addSkyBox(ISceneManager * scene, 
+			ITexture * top,
+			ITexture * bottom,
+			ITexture * left,
+			ITexture * right,
+			ITexture * front,
+			ITexture * back) {
+		return scene->addSkyBoxSceneNode(top, bottom, left, right, front, back);
+	}
+
+	ICameraSceneNode * addCamera(ISceneManager * scene) {
+		return scene->addCameraSceneNode();
+	}
+
+	void setTarget(ICameraSceneNode * camera, Vec3 target) {
+		camera->setTarget(vector3d<f32>(target.x, target.y, target.z));
+	}
+
+	void setPosition(ISceneNode * node, Vec3 position) {
+		node->setPosition(vector3d<f32>(position.x, position.y, position.z));
+	}
+
+	void setRotation(ISceneNode * node, Vec3 rotation) {
+		node->setRotation(vector3d<f32>(rotation.x, rotation.y, rotation.z));
+	}
+
+	void setScale(ISceneNode * node, Vec3 scale) {
+		node->setScale(vector3d<f32>(scale.x, scale.y, scale.z));
+	}
+
+	void setMaterialFlag(ISceneNode * node, E_MATERIAL_FLAG flag, int value) {
+		node->setMaterialFlag(flag, value);
+	}
+
+	void setMaterialTexture(ISceneNode * node, int layer, ITexture * texture) {
+		node->setMaterialTexture(layer, texture);
 	}
 
 }
